1a43b23c13c3320faf0dc0bfa0a47fc9a9eda4dc
Shinya Todaka <tdk0105.strawberry@gmail.com>
2020-10-22 18:44:13 +0900

    fix

--- Sources/KumadaiPortal/Model/CourseResponse.swift
+++ Sources/KumadaiPortal/Model/CourseResponse.swift
@@ -148,7 +148,7 @@ public struct Day: Decodable {
 public struct Period: Decodable {
     public let courses: [Course]
     
-    init(courses: [Course]) {
+    public init(courses: [Course]) {
         self.courses = courses
     }
 }
@@ -158,7 +158,7 @@ public struct Course: Decodable {
     public let name: String
     public let lms: Int
     
-    init(courseId: String, name: String, lms: Int) {
+    public init(courseId: String, name: String, lms: Int) {
         self.courseId = courseId
         self.name = name
         self.lms = lms
